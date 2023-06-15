ade990bb303cc6bdf6a34ac6337ee11cef551cd0
Dika <49613928+dikako@users.noreply.github.com>
2023-06-15 21:55:00 +0700

    Updat element

--- src/main/java/stockbit/test/page_object/LoginPage.java
+++ src/main/java/stockbit/test/page_object/LoginPage.java
@@ -6,19 +6,19 @@ import stockbit.test.base.BasePageObject;
 public class LoginPage extends BasePageObject {
 
     public void clickLoginEntryPoint() {
-        isDisplayed(By.id("com.stockbit.android:id/ivFragmentStreamCompanyLogo"));
+        isDisplayed(By.id("com.stockbit.android:id/ini id gua ngapa lo???"));
         tap(By.id("com.stockbit.android:id/btnWellcomeLogIn"));
     }
 
     public void inputUsername(String username) {
-        inputText(By.xpath("(//android.widget.EditText[contains(@resource-id, 'tiet_text_field_input')])[1]"), username);
+        inputText(By.xpath("(//android.widget.EditText[contains(@resource-id, "), username);
     }
 
     public void inputPassword(String password) {
-        inputText(By.xpath("(//android.widget.EditText[contains(@resource-id, 'tiet_text_field_input')])[2]"), password);
+        inputText(By.xpath("(//android.widgetid, 'tiet_text_field_input')])[2]"), password);
     }
 
     public void clickButtonLogin() {
-        tap(By.id("com.stockbit.android:id/btn_login"));
+        tap(By.id("com.stockbit.abtn_login"));
     }
 }
